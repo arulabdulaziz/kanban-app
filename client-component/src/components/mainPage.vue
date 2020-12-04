46cae86e26f885b8a6e4c095f420a17d7292788a
arulabdulaziz <rulz.matrixs@gmail.com>
2020-12-04 13:49:17 +0700

    add task using modals

--- client-component/src/components/mainPage.vue
+++ client-component/src/components/mainPage.vue
@@ -1,34 +1,46 @@
 <template>
   <div>
       <Navbar @logout="logout"></Navbar>
-      <form class="form-inline" id="todos-form" method="POST" @submit.prevent="addtask">
-        <label class="sr-only" for="title">Title</label>
-        <input type="text" class="form-control mb-2 mr-sm-2" id="title" name="title" placeholder="Title" v-model="title">
-      
-        <label class="sr-only" for="description">Description</label>
-        <div class="input-group mb-2 mr-sm-2">
-          <div class="input-group-prepend">
-          </div>
-          <input type="textarea" class="form-control" id="description" name="description" placeholder="Description" v-model="description">
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#form">
+                Add Task
+            </button>
+        <div class="modal fade" id="form" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header border-bottom-0">
+                <h5 class="modal-title" id="exampleModalLabel">Edit Task</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <form @submit.prevent="addTask">
+                <div class="modal-body">
+                    <div class="form-group">
+                    <label>Title</label>
+                    <input type="text" class="form-control" aria-describedby="emailHelp" v-model="title">
+                    </div>
+                    <div class="form-group">
+                    <label>Description</label>
+                    <input type="textarea" class="form-control" v-model="description">
+                    </div>
+                    <div class="form-group">
+                    <label>Category</label>
+                    <select name="category" id="" class="form-control" v-model="category">
+                        <option value="disabled" disabled>--SELECT CATEGORY--</option>
+                        <option value="backlog">Backlog</option>
+                        <option value="todo">Todo</option>
+                        <option value="doing">Doing</option>
+                        <option value="done">Done</option>
+                    </select>
+                    </div>
+                </div>
+                <div class="modal-footer border-top-0 d-flex justify-content-center">
+                    <button type="submit" class="btn btn-success">Submit</button>
+                </div>
+                </form>
+            </div>
         </div>
-
-        <label class="sr-only" for="category">Category</label>
-        <div class="input-group mb-2 mr-sm-2">
-          <div class="input-group-prepend">
-          </div>
-          <select name="category" id="" class="form-control" v-model="category">
-            <option value="disabled" disabled>--SELECT CATEGORY--</option>
-            <option value="backlog">Backlog</option>
-            <option value="todo">Todo</option>
-            <option value="doing">Doing</option>
-            <option value="done">Done</option>
-          </select>
-        </div>
-        <button type="submit" class="btn btn-primary mb-2" id="btn-submit-todo">Add Kanban</button>
-      </form>
-      <!-- <ul>
-          <li v-for="(task, index) in tasks" :key="index">{{task}}</li>
-      </ul> -->
+      </div>
       <div class="container">
         <div class="row mt-4 d-flex justify-content-around">
             <Category v-for="(category, index) in categories" :key="index" :categoryTask="category" :tasks="tasks"
@@ -68,7 +80,7 @@ export default {
             // console.log(`logout from main page`);
             this.$emit('logout')
         },
-        addtask(){
+        addTask(){
             const obj = {
                 title: this.title,
                 description: this.description,
@@ -78,6 +90,7 @@ export default {
             this.description = '',
             this.category = ''
             this.$emit('addTask', obj)
+            $('#form').modal('hide')
         },
         editDisplay(id){
             axios({
