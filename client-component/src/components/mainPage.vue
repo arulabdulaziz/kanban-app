98f63ea920c2aae41fd170629eb16db84f5a7730
arulabdulaziz <rulz.matrixs@gmail.com>
2020-12-04 21:12:51 +0700

    documentation and deploy

--- client-component/src/components/mainPage.vue
+++ client-component/src/components/mainPage.vue
@@ -10,7 +10,7 @@
             <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header border-bottom-0">
-                <h5 class="modal-title" id="exampleModalLabel">Edit Task</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Add Task</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
